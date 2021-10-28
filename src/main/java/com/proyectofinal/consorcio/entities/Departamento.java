36e22d08c664e4985c283faf5e1125d92faa9b1f
Pedro de Moya <pedrodemoya@gmail.com>
2021-10-28 20:00:26 -0300

    Version con todas las entidades creadas

--- src/main/java/com/proyectofinal/consorcio/entities/Departamento.java
+++ src/main/java/com/proyectofinal/consorcio/entities/Departamento.java
@@ -1,20 +1,24 @@
 package com.proyectofinal.consorcio.entities;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import javax.persistence.OneToOne;
+
+import org.hibernate.annotations.GenericGenerator;
 
 @Entity
 public class Departamento {
 
 	@Id
+	@GeneratedValue(generator = "uuid")
+	@GenericGenerator(name = "uuid", strategy = "uuid2")
 	private String id;
+	private String usuario;
 	private String password;
-	private Integer piso;
+	private int piso;
 	private String dpto;
 	private Double porcentajeParticipacion;
-	
-	
+		
 	//GETTERS Y SETTERS
 
 	public String getId() {
@@ -25,6 +29,22 @@ public class Departamento {
 		this.id = id;
 	}
 
+	public String getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+
+	public void setPiso(int piso) {
+		this.piso = piso;
+	}
+
+	public int getPiso() {
+		return piso;
+	}
+
 	public Double getPorcentajeParticipacion() {
 		return porcentajeParticipacion;
 	}
@@ -33,10 +53,21 @@ public class Departamento {
 		this.porcentajeParticipacion = porcentajeParticipacion;
 	}
 
+	public String getPassword() {
+		return password;
+	}
 
-	
+	public void setPassword(String password) {
+		this.password = password;
+	}
 
-	
+	public String getDpto() {
+		return dpto;
+	}
+
+	public void setDpto(String dpto) {
+		this.dpto = dpto;
+	}
 	
 
 }
--- src/main/java/com/proyectofinal/consorcio/entities/Edificio.java
+++ src/main/java/com/proyectofinal/consorcio/entities/Edificio.java
@@ -1,20 +1,30 @@
 package com.proyectofinal.consorcio.entities;
 
+import java.util.List;
+
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+
+import org.hibernate.annotations.GenericGenerator;
 
 @Entity
 public class Edificio {
 
 	@Id
+	@GeneratedValue(generator = "uuid")
+	@GenericGenerator(name = "uuid", strategy = "uuid2")
 	private String id;
 	private String direccion;	
+	private Boolean alta;
 	
+	@OneToOne
 	private Expensas expensas;
 
 	@OneToMany
-	private Departamento departamento;
+	private List<Departamento> departamentos;
 	
 	//GETTERS Y SETTERS
 
@@ -26,6 +36,14 @@ public class Edificio {
 		this.id = id;
 	}
 
+	public Boolean getAlta() {
+		return alta;
+	}
+
+	public void setAlta(Boolean alta) {
+		this.alta = alta;
+	}
+
 	public String getDireccion() {
 		return direccion;
 	}
@@ -33,15 +51,15 @@ public class Edificio {
 	public void setDireccion(String direccion) {
 		this.direccion = direccion;
 	}
-
-	public Departamento getDepartamento() {
-		return departamento;
-	}
-
-	public void setDepartamento(Departamento departamento) {
-		this.departamento = departamento;
-	}
 	
+	public List<Departamento> getDepartamentos() {
+		return departamentos;
+	}
+
+	public void setDepartamentos(List<Departamento> departamentos) {
+		this.departamentos = departamentos;
+	}
+
 	public Expensas getExpensas() {
 		return expensas;
 	}
@@ -50,6 +68,4 @@ public class Edificio {
 		this.expensas = expensas;
 	}
 	
-	
-
 }

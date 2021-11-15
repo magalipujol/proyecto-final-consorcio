a6fad612ab614809795e6b8a8ce61a3a5b21a6e4
magalipujol <magalicpujol@gmail.com>
2021-11-15 20:23:21 -0300

    cambios en la navbar agregué condicional del botón 'volver'

--- src/main/resources/templates/js/VueApp.js
+++ src/main/resources/templates/js/VueApp.js
@@ -1,26 +1,27 @@
 let app = Vue.createApp({
-    data: function () {
-      return {
+  data: function () {
+    return {};
+  },
+});
 
-      };
-    },
-  });
-
-  app.component("navbar", {
-    template: /* html */ `
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+app.component("navbar", {
+  props: {
+    backBtn: Boolean,
+  },
+  template: /* html */ `
+    <nav class="navbar navbar-expand-lg fixed-top navbar-light bg-light">
   <a class="navbar-brand" href="#">EGGPENSAS</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
   <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
     <div class="navbar-nav">
-      <a class="nav-item nav-link active" href="#">VOLVER<span class="sr-only">(current)</span></a>
+      <a :v-if="backBtn" class="nav-item nav-link active" href="#">VOLVER</a>
       <a class="nav-item nav-link" href="#">LOG OUT</a>
     </div>
   </div>
 </nav>
       `,
-  });
+});
 
 app.mount("#app");

7c24ee2f8931cc738e492a6a9e10c58ef5428e58
magalipujol <magalicpujol@gmail.com>
2021-11-15 21:34:18 -0300

    agregué una alerta para cerrar la sesion

--- src/main/resources/templates/js/VueApp.js
+++ src/main/resources/templates/js/VueApp.js
@@ -9,8 +9,13 @@ app.component("navbar", {
     backBtn: Boolean,
     backBtnRef: String,
   },
+  methods: {
+    alertLogOut: function () {
+      confirm('Está seguro que desea cerrar su sesión?'); ;
+    },
+  },
   template: /* html */ `
-    <nav class="navbar navbar-expand-lg fixed-top navbar-light bg-light">
+    <nav class="navbar navbar-expand-lg sticky-top navbar-light bg-light">
   <a class="navbar-brand" href="#">EGGPENSAS</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
@@ -19,8 +24,8 @@ app.component("navbar", {
   <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
     <div class="navbar-nav">
 
-      <a :v-if="backBtn" class="nav-item nav-link active" :href="backBtnRef">VOLVER  {{ backBtnRef }} fsd</a>
-      <a class="nav-item nav-link" href="#">LOG OUT</a>
+      <a :v-if="backBtn" class="nav-item nav-link active" :href="backBtnRef">VOLVER  {{ backBtnRef }}</a>
+      <a class="nav-item nav-link" href="login.html" v-on:click="alertLogOut">LOG OUT</a>
     </div>
   </div>
 </nav>
